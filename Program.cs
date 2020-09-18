ff6d33ecf26f3879de23f322f584bfb58df41d6a
yiiow1309 <55523982+yiiow1309@users.noreply.github.com>
2020-09-18 09:34:02 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LJS_Report2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int num1;
+
+            num = 4;
+            num1 = 2;
+
+            Console.WriteLine(num + num1);
+            Console.WriteLine(num - num1);
+            Console.WriteLine(num * num1);
+            Console.WriteLine(num / num1);
+
+
+        }
+    }
+}
